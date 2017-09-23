29832c678aef6b0f7af2b2f1ef46c3afd9550b2c
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2017-09-23 14:02:22 -0500

    POST questionnaire added

--- models/input.js
+++ models/input.js
@@ -1,1 +1,12 @@
+let mongoose = require('mongoose');
+let InputSchema = mongoose.Schema({
+  date: Date(),
+  answers: {},
+  user: {
+    zip: Number,
+    name: String,
+    email: String,
+  },
+});
 
+module.exports = mongoose.model('Input', InputSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,49 @@
 var express = require('express');
 var Input = require('../models/input')
-var formidable = require('formidable');
 
 var router = express.Router();
 
-router.get("/questionnaire", function(req, res, next) {
-  res.json([
-    "Did you eat yesterday?": ["yes", "no"],
-    "Have you had difficulty getting food in the past 2 weeks": ["yes", "no"],
-  ]);
-});
+/*router.get("/questionnaire", function(req, res, next) {
+  res.json({
+    "Did you eat yesterday?": {
+      id: "q1",
+      ans: ["yes", "no"],
+      required: "true"
+    },
+    "Have you had difficulty getting food in the past year?": {
+      id: "q2",
+      ans: ["yes", "no"],
+      required: "true"
+    },
+    "Have you had difficulty getting food in the past month?": {
+      id: "q2",
+      ans: ["yes", "no"],
+      required: "true"
+    },
+    "Zip Code": {
+      id: "zip",
+      required: "true"
+    },
+    "Name": {
+      id: "name",
+      required: "false"
+    }
+  });
+});*/
 
 router.post("/questionnaire", function(req, res, next) {
-  
+  var newInput = new Input();
+  newInput.date = new Date();
+  newInput.user.name = req.body.name;
+  newInput.user.zip = req.body.zip;
+  newInput.user.email = req.body.email;
+  delete req.body.name;
+  delete req.body.zip;
+  delete req.body.email;  
+  newInput.answers = req.body;
+  Input.save(newInput, function(err) {
+    if (err) throw err;
+  });
 });
 
 module.exports = router;
