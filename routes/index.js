06fe37119ca2ff678852fb9d6ce4bce28e701af1
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2017-09-23 13:10:14 -0500

    GET questionnaire added

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,18 @@
+var express = require('express');
+var Input = require('../models/input')
+var formidable = require('formidable');
 
+var router = express.Router();
+
+router.get("/questionnaire", function(req, res, next) {
+  res.json([
+    "Did you eat yesterday?": ["yes", "no"],
+    "Have you had difficulty getting food in the past 2 weeks": ["yes", "no"],
+  ]);
+});
+
+router.post("/questionnaire", function(req, res, next) {
+  
+});
+
+module.exports = router;
